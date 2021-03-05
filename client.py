3e866bbdfe80bc4f07e091608b907c4faaf3e10c
Maestro <matteo.ghia.2001@gmail.com>
2021-03-05 13:25:30 +0100

    boh

--- client.py
+++ client.py
@@ -1,3 +1,22 @@
-import websocket
+import asyncio
+import websockets
 
-ws = websocket.create_connection('ws://servercatta.herokuapp.com/')
+
+async def hello():
+    role = str(input("inserire il ruolo:\t"))
+
+    uri = f"ws://servercatta.herokuapp.com/{role}"
+    async with websockets.connect(uri) as ws:
+        inp = 'ciao'
+        while inp != 'end':
+            inp = str(input("inp:\t"))
+
+            if inp == 'recv':
+                print(await ws.recv())
+
+            elif inp == 'send':
+                s = str(input("send:\t"))
+                await ws.send(s)
+
+
+asyncio.get_event_loop().run_until_complete(hello())
